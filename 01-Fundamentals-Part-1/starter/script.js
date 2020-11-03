0774a16e9b077e461867c4c4c58084bcd14d44bf
Jonathan Abbott <jonathan@waxedmedia.co.za>
2020-11-03 15:59:10 +0200

    Boolean Logic

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,59 @@
+/*
+const mark = {
+    firstName: `Mark`,
+    yearBirth: `1981`,
+    gender: `Male`,
+    weight: 95,
+    height: 1.88
+}
+
+const john = {
+    firstName: `John`,
+    yearBirth: `1951`,
+    gender: `Male`,
+    weight: 85,
+    height: 1.76,
+}
+
+function bmiCalc(weight,height) {
+    bmi = weight / (height ** 2)
+    return bmi; 
+}
+
+
+let markBmi = bmiCalc(mark.weight, mark.height);
+let johnBmi = bmiCalc(john.weight, john.height);
+
+console.log(`Johns BMI: ${johnBmi}
+Marks BMI: ${markBmi}`);
+
+if (markBmi > johnBmi) {
+    console.log(`${mark.firstName}'s BMI is higher than ${john.firstName}'s with a value of ${markBmi}`);
+}
+else if (johnBmi > markBmi) {
+    console.log(`${john.firstName}'s BMI is higher than ${mark.firstName}'s with a value of ${johnBmi}` ); // Template Literal
+}
+else {
+    console.log(`${john.firstName} & ${mark.firstName} are fat!!!` );
+}
+
+
+console.log(Number(john.yearBirth), john.yearBirth);
+console.log(String(john.weight) + 1);
+console.log(john.weight + 1);
+
+// 5 Falsy Values: 0, '', null, undefined, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(1));
+console.log(Boolean(``));
+console.log(Boolean(`Joan`));
+console.log(Boolean({}));
+
+let favourite = Number(prompt(`What is your favourite number?`));
+
+if (favourite === 7) console.log(`That is my waterpolo number`);
+else if (favourite === 3) console.log(`That is my hockey number`);
+
+*/
+
